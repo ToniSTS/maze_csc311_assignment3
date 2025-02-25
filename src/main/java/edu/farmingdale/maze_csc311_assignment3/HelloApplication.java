7c2a12c6f9e74e37012a4452ddde25f8013256c8
Patrick Lomangino <lomapj@farmingdale.edu>
2025-02-24 20:34:28 -0500

    Commit, Added walls and second maze view.

--- src/main/java/edu/farmingdale/maze_csc311_assignment3/HelloApplication.java
+++ src/main/java/edu/farmingdale/maze_csc311_assignment3/HelloApplication.java
@@ -1,45 +1,83 @@
 package edu.farmingdale.maze_csc311_assignment3;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 600, 400);
 
-        HelloController controller = fxmlLoader.getController();
+    private PixelReader pixelReader;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 800, 600);
+        stage.setScene(scene);
+        stage.setTitle("Maze Demo - Final Version");
+        stage.show();
+
+        HelloController controller = loader.getController();
+
+        ImageView maze1View = controller.getMaze1View();
+
         ImageView robotView = controller.getRobotView();
 
-        // Key Event Handling for Robot Movement
-        scene.setOnKeyPressed(event -> {
-            switch (event.getCode()) {
-                case UP:
-                    robotView.setY(robotView.getY() - 5);
-                    break;
-                case DOWN:
-                    robotView.setY(robotView.getY() + 5);
-                    break;
-                case LEFT:
-                    robotView.setX(robotView.getX() - 5);
-                    break;
-                case RIGHT:
-                    robotView.setX(robotView.getX() + 5);
-                    break;
+        pixelReader = maze1View.getImage().getPixelReader();
+
+
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
+            if (e.getCode() == KeyCode.UP
+                    || e.getCode() == KeyCode.DOWN
+                    || e.getCode() == KeyCode.LEFT
+                    || e.getCode() == KeyCode.RIGHT) {
+                e.consume();
+
+                double step = 5;
+                double x = robotView.getLayoutX();
+                double y = robotView.getLayoutY();
+                double newX = x, newY = y;
+
+                switch (e.getCode()) {
+                    case UP -> newY -= step;
+                    case DOWN -> newY += step;
+                    case LEFT -> newX -= step;
+                    case RIGHT -> newX += step;
+                    default -> {}
+                }
+
+                double centerX = newX + 12;
+                double centerY = newY + 12;
+
+                if (isPath(centerX, centerY)) {
+                    robotView.setLayoutX(newX);
+                    robotView.setLayoutY(newY);
+                } else {
+                    System.out.println("Blocked at " + centerX + "," + centerY);
+                }
             }
         });
 
-        stage.setTitle("Robot");
-        stage.setScene(scene);
-        stage.show();
+        Platform.runLater(() -> root.requestFocus());
+    }
+
+
+    private boolean isPath(double x, double y) {
+
+        if (x < 0 || y < 0 || x >= 600 || y >= 400) {
+            return false;
+        }
+        Color c = pixelReader.getColor((int)x, (int)y);
+        return c.getRed() > 0.9 && c.getGreen() > 0.9 && c.getBlue() > 0.9;
     }
 
     public static void main(String[] args) {
--- src/main/java/edu/farmingdale/maze_csc311_assignment3/HelloController.java
+++ src/main/java/edu/farmingdale/maze_csc311_assignment3/HelloController.java
@@ -1,18 +1,30 @@
 package edu.farmingdale.maze_csc311_assignment3;
 
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
+import javafx.scene.control.TabPane;
 import javafx.scene.image.ImageView;
 
 public class HelloController {
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private ImageView maze1View;
+
     @FXML
     private ImageView robotView;
 
-    public ImageView getRobotView() {
-        return robotView;
+    @FXML
+    public void initialize() {
+        tabPane.setFocusTraversable(false);
     }
 
-    public void initialize() {
-        robotView.setImage(new Image(getClass().getResource("/edu/farmingdale/maze_csc311_assignment3/robot.png").toExternalForm()));
+    public ImageView getMaze1View() {
+        return maze1View;
+    }
+
+    public ImageView getRobotView() {
+        return robotView;
     }
 }
